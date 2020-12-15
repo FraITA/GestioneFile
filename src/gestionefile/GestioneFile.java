604ab7fa2aa6607cb86799dfc3532cf0b27bedff
FraITA <user@DESKTOP-K02RLIM>
2020-12-15 09:46:23 +0100

    Aggiunto inserimento da tastiera

--- src/gestionefile/GestioneFile.java
+++ src/gestionefile/GestioneFile.java
@@ -5,6 +5,12 @@
  */
 package gestionefile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author user
@@ -18,8 +24,38 @@ public class GestioneFile {
 	 */
 	public static void main(String[] args) {
 			GestoreFile gestore = new GestoreFile();
+			
+			InputStreamReader inputReader = new InputStreamReader(System.in);
+
+			BufferedReader bufferedReader = new BufferedReader(inputReader);
+			
 			ThreadLettore tr = new ThreadLettore(gestore);
 			ThreadScrittore tw = new ThreadScrittore(gestore);
+			
+			String ext = "";
+			boolean flag = false;
+			String[] exts = {"xml", "csv", "json"};
+			String str = "";
+					
+			do{
+				try {
+					System.out.println("Inserire nome file (con estensione XML, JSON o CSV)");
+					str = bufferedReader.readLine();
+					
+					for(String s : exts){
+						ext = str.split(".")[1];
+						if(ext.equals(s)){
+							flag = true;
+						}
+					}
+					
+					if(!flag){
+						System.err.println("Inserimento non valido");
+					}
+				} catch (IOException ex) {
+					Logger.getLogger(GestioneFile.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}while(!flag);
 			
 			tr.start();
 			tw.start();
